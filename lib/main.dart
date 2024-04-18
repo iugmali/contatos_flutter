fb3bc1c89115bd5ee6725d3a21375757cf1a785d
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2024-04-18 09:58:56 -0300

    style - adjust theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:contatos_flutter/models/contato_model.dart';
 import 'package:contatos_flutter/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
@@ -9,6 +10,8 @@ void main() async {
   Hive.registerAdapter(ContatoModelAdapter());
   runApp(const MyApp());
 }
+
+var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.green);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -19,8 +22,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Contatos App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      theme: ThemeData().copyWith(
+          scaffoldBackgroundColor: const Color.fromARGB(255, 189, 255, 252),
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: GoogleFonts.jetBrainsMono(
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+              color: kColorScheme.onSecondaryContainer,
+            ),
+          )
       ),
       home: const SplashScreen(),
     );
